e2d070d711881f97d066601e3379684e0307811e
tahachaudhry <taha@nyu.edu>
2022-06-14 19:57:31 -0500

    initial commit

--- Develop/script.js
+++ Develop/script.js
@@ -13,5 +13,43 @@ function writePassword() {
 
 }
 
+function generatePassword() {
+  var numChars ="0123456789";
+  var upperCases = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var lowerCases = upperCases.toLowerCase();
+  var specChars = "!@#$%^&*()_=+";
+  var newPass = "";
+
+  var passLength = Number(prompt("Please type your password of character length (8-128 characters)."));
+
+  if (passLength >= 8 && passLength <=128){
+    var passUpperCase = confirm("Does your password require uppercase characters?");
+    var passLowerCase = confirm("Does your password require lowercase characters");
+    var passNum = confirm("Does your password require numbers?");
+    var passSpec = confirm("Does your password require speical characters?");
+    for (i=0; i < passLength; i++){
+      if(newPass.length < passLength){
+        if (passUpperCase){
+          newPass += upperCases[Math.floor(Math.random() * upperCases.length)];
+        } if (passLowerCase){
+          newPass += lowerCases[Math.floor(Math.floor() * upperCases.length)];
+        } if (passNum) {
+          newPass += numChars[Math.floor(Math.random() * numChars.length)];
+        } if (passSpec){
+          newPass += specChars[Math.floor(Math.random() * specChars.length)];
+        }
+      } else {
+        break;
+      }
+    }
+    return newPass;
+  }
+  else {
+    alert("ERROR: input must be a number between 8 and 128. Try again.");
+    return "Try Again";
+  }
+
+}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
